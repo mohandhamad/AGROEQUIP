f9b2af2ca7e0d784a9aa859073b27319485d1035
Mohand Hamad <120589054+mohandhamad@users.noreply.github.com>
2024-10-20 11:07:40 +0400

    Delete LAB2 Project directory

--- LAB2 Project/js/mail.js
+++
@@ -1,8 +0,0 @@
-function sendMail(){
-	let parms = {
-	name : document.getElementById("name").value,
-	email : document.getElementById("email").value,
-	message : document.getElementById("message").value,
-	}
-	emailjs.send("service_2foz3jd","template_2841dpk",parms).then(alert("Email Sent!!"))
-}
